b6cc189d4de7b4e1e4463c4cbfc522e290391c67
Martin Evans <martindevans@gmail.com>
2021-06-10 21:21:35 +0100

    Added some acid tests

---
+++ Yolol.IL.Tests/Acid/StringLogic.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Yolol.IL.Tests.TestHelpers;
+
+namespace Yolol.IL.Tests.Acid
+{
+    [TestClass]
+    public class StringLogic
+    {
+        [TestMethod]
+        public void AcidStringLogic()
+        {
+            var (ms, _) = Test(new[] {
+                    $"num=1 if \"\" then goto 19 end num++",
+                    "if \"abc\" then goto 19 end num++",
+                    "if \"1\" then goto 19 end num++",
+                    "if \"0\" then goto 19 end num++",
+                    "if not \"\" then goto 19 end num++",
+                    "if not \"1\" then goto 19 end num++",
+                    "if not \"0\" then goto 19 end num++",
+                    "if 1 and \"\" then goto 19 end num++",
+                    "if 1 and \"1\" then goto 19 end num++",
+                    "if 1 and \"0\" then goto 19 end num++",
+                    "if not (1 or \"\") then goto 19 end num++",
+                    "if not (1 or \"1\") then goto 19 end num++",
+                    "if not (1 or \"0\") then goto 19 end num++",
+                    "if 0 or \"\" then goto 19 end num++",
+                    "if 0 or \"1\" then goto 19 end num++",
+                    "if 0 or \"0\" then goto 19 end num++",
+                    "if num != 17 then OUTPUT=\"Skipped: \"+(17-num)+\" tests\" goto 20 end",
+                    "OUTPUT=\"ok\" goto20",
+                    "output=\"Failed test #\"+num",
+                    "goto20"
+                },
+                30
+            );
+
+            Assert.AreEqual("ok", ms.GetVariable("OUTPUT").String.ToString());
+        }
+    }
+}
